3fe46ae7e025c0bf92cf4723876b6f80d03dfa7b
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-15 03:11:15 -0700

    adding printf.c file

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+#endif
---
+++ printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _printf - calculates the number of characters printed
+ * @format:  is a character string to be printed
+ *
+ * Return:  the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, j, k, lenStr, lenFormat = strlen(format);
+	char *buffer, *str;
+	va_list allargs;
+
+	buffer = (char *)malloc(lenFormat + 1);
+	if (buffer == NULL)
+		exit(98); //indicating memory allocation failure
+	i = 0;
+	k = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i = i + 1;
+			switch (format[i])
+			{
+				case 'c':
+					buffer[k] = va_arg(allargs, int);
+					k++;
+					break;
+				case 's':
+					str = va_arg(allargs, char*);
+					lenStr = strlen(str);
+					for (j = k; j < (lenStr + k); j++)
+					{
+						buffer[k] = str[j - k];
+						k++;
+					}
+					break;
+				case '%':
+					buffer[k] = av_arg(allargs, int);
+					k++;
+					break;
+			}
+		}
+
+	}
+}
