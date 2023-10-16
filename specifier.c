2b551f4912943413760f3a8551c336cccb46d6af
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-16 06:50:21 -0700

    w

---
+++ specifier.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+/**
+ * get_specifier - finds the format func
+ * @s: the format string
+ *
+ * Return: the number of bytes printed
+ */
+
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: the paramters struct
+ * Return: if flag was valid
+ */
+
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flage = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i)
+}
+
+/**
+ * get_width - gets the width from the format string
+ * @s: the format string
+ * @params: the paramter struct
+ * @ap: the argument pointer
+ *
+ * Return: new pointer
+ */
+
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
