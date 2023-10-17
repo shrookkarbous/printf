8ad38069804c20234a06b50f627686198deca6e4
Jihan Ahmed <jihanahmed41@gmail.com>
2023-10-17 05:09:22 -0700

    updating print_int.c file

--- printf.c
+++ printf.c
@@ -18,33 +18,26 @@ int _printf(const char *format, ...)
 	va_start(allargs, format);
 	while (*(format + i))
 	{
-		if (format[i++] == '%')
+		if (format[i] == '%')
 		{
+			i = i + 1;
 			if (format[i] == '\0')
 			{	va_end(allargs);
 				return (-1); }
-			switch (format[i])
+			if (format[i] == 'c')
+				BytesNum += print_char(allargs);
+			else if (format[i] == 's')
+				BytesNum += print_string(allargs);
+			else if (format[i] == '%')
+				BytesNum += _putchar(format[i]);
+			else if (format[i] == 'i' || format[i] == 'd')
 			{
-				case 'c':
-					BytesNum += print_char(allargs);
-					break;
-				case 's':
-					BytesNum += print_string(allargs);
-					break;
-				case '%':
-					BytesNum += _putchar(format[i]);
-					break;
-				case 'i':
-					num = va_arg(allargs, int);
-					BytesNum += print_int(num);
-					break;
-				case 'd':
-					num = va_arg(allargs, int);
-					BytesNum += print_int(num);
-					break;
-				default:
-					BytesNum += _putchar(format[--i]);
-					BytesNum += _putchar(format[++i]); }}
+				num = va_arg(allargs, int);
+				BytesNum += print_int(num); }
+			else
+			{
+				BytesNum += _putchar(format[--i]);
+				BytesNum += _putchar(format[++i]); }}
 		else
 			BytesNum += _putchar(format[i]);
 		i++; }
